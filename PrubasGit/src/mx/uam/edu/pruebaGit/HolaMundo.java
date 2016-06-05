dc17734e9916ed42441f3b6d4198ae20622e942a
ubuntu <ubuntu@ubuntu-UX32A>
2016-06-05 17:46:38 -0500

    IsaacMG2016: Clase de prueba Netbeans-Git

---
+++ PrubasGit/src/mx/uam/edu/pruebaGit/HolaMundo.java
@@ -0,0 +1,14 @@
+package mx.uam.edu.pruebaGit;
+
+/**
+ *
+ * @author Isaac MG
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo\n");
+    }
+    
+}
